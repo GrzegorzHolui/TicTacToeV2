602f1c0a85c034b8da48fcdac081a64a1932d769
grzes <grzesiu.holui@gmail.com>
2023-05-24 12:01:36 +0200

    add class PositionAIGenerator and create method positionGenerator

---
+++ src/main/java/PositionAIGenerator/PositionAIGenerator.java
@@ -0,0 +1,13 @@
+package PositionAIGenerator;
+
+import GameState.GameStateFacade;
+import PositionReceiver.dto.PositionDto;
+
+public class PositionAIGenerator {
+
+    GameStateFacade gameStateFacade;
+
+    public PositionDto positionGenerator() {
+        throw new RuntimeException("Should be here code of Algorithm Min-MAX");
+    }
+}
